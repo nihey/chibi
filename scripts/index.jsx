f73e3fdb4d20220cd61a592453f84a7f662d7abe
Nihey Takizawa <nihey.takizawa@gmail.com>
2016-07-16 00:04:18 -0300

    Initial empty boilerplate

---
+++ scripts/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let Index = React.createClass({
+  render: function() {
+    return <div></div>;
+  },
+});
+
+ReactDOM.render(<Index/>, document.getElementById('react-body'));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    IndexHtmlPlugin = require('indexhtml-webpack-plugin'),
+    path = require('path');
+
+var cssExtractTextPlugin = new ExtractTextPlugin('[contenthash].css');
+
+module.exports = {
+  entry: {
+    'script': './scripts/index.jsx',
+    'index.html': './index.html',
+  },
+
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader'},
+      { test: /\.jsx?$/, exclude: /(node_modules|bower_components)\//, loader: 'babel-loader'},
+      { test: /\.(ttf.*|eot.*|woff.*|ogg|mp3)$/, loader: 'file-loader'},
+      { test: /.(png|jpe?g|gif|svg.*)$/, loader: 'file-loader!img-loader?optimizationLevel=7&progressive=true'},
+      {
+        test: /\.css$/,
+        loader: cssExtractTextPlugin.extract('style-loader', 'css-loader'),
+      },
+      {
+        test: /\.less$/,
+        loader: cssExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
+      },
+      {
+        test: /\.html$/,
+        loader: 'html?attrs=link:href img:src',
+      },
+    ],
+  },
+
+  plugins: [
+    cssExtractTextPlugin,
+    new IndexHtmlPlugin('index.html', 'index.html'),
+    new webpack.DefinePlugin({
+      Environment: JSON.stringify(require('config')),
+    }),
+  ],
+
+  resolve: {
+    root: path.join(__dirname, 'scripts'),
+    extensions: ['', '.js', '.json'],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+};
